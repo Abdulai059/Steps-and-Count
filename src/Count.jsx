97556a53eb3c9e8e97a5ee63027fb0bdce819161
Abdulai059 <abdulaiosman8080@gmail.com>
2025-08-08 00:59:19 +0000

    update

--- src/Count.jsx
+++ src/Count.jsx
@@ -2,55 +2,68 @@ import { useState } from "react";
 import "../src/index.css";
 
 function Count() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
 
   // Get today's date
   const today = new Date();
 
-  const futureDate = new Date(today);
-  futureDate.setDate(today.getDate() + count);
+  const date = new Date("Aug 7 2025");
+  date.setDate(today.getDate() + count);
 
-  const formattedDate = futureDate.toDateString();
-
-  function handleStepPlus() {
-    {
-      setStep((s) => s + 1);
-    }
-  }
-  function handleStepMinus() {
-    {
-      setStep((s) => Math.max(s - 1, 1));
-    }
-  }
+  // function handleStepPlus() {
+  //   {
+  //     setStep((s) => s + 1);
+  //   }
+  // }
+  // function handleStepMinus() {
+  //   {
+  //     setStep((s) => s - 1);
+  //   }
+  // }
 
   function handleCountPlus() {
     {
-      setCount((c) => c + 1);
+      setCount((c) => c + step);
     }
   }
   function handleCountMinus() {
     {
-      setCount((c) => c - 1);
+      setCount((c) => c - step);
     }
   }
   return (
     <>
       <div className="steps">
         <div className="count">
-          <button
+          {/* <button
             style={{ backgroundColor: "#7950f2", color: "#fff" }}
             onClick={handleStepMinus}
           >
             -
-          </button>
+          </button> */}
+
+          {/* Implentiting the range slider */}
+          <input
+            class="slider"
+            type="range"
+            min="0"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          />
           <span className="step-message">step: {step}</span>
-          <button
+          {/* <button
             style={{ backgroundColor: "#7950f2", color: "#fff" }}
             onClick={handleStepPlus}
           >
             +
-          </button>
+          </button> */}
         </div>
 
         <div className="count">
@@ -60,7 +73,15 @@ function Count() {
           >
             -
           </button>
-          <span className="count-message">Count: {count}</span>
+          {/* Inplementing the count slider */}
+          <input
+            className="no-spinner"
+            type="number"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+
+          {/* <span className=" ">Count: {count}</span> */}
           <button
             style={{ backgroundColor: "#7950f2", color: "#fff" }}
             onClick={handleCountPlus}
@@ -70,8 +91,28 @@ function Count() {
         </div>
 
         <p className="message">
-          {count} days from today is <strong>{formattedDate}</strong>
+          <span>
+            {count === 0
+              ? "Today is"
+              : count > 0
+              ? `${count} days from today is`
+              : `${Math.abs(count)} days ago was`}
+          </span>
+          <span>{date.toDateString()}</span>
         </p>
+
+        {/* Implementing Reset Button */}
+
+        {count !== 0 || step !== 1 ? (
+          <div className="reset">
+            <button
+              onClick={handleReset}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Reset
+            </button>
+          </div>
+        ) : null}
       </div>
     </>
   );
